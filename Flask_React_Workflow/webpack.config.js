947ab3bd11e157bfca4b1f644721484ad00bf052
Tianning Li <ltianningli@gmail.com>
2020-12-06 18:13:25 -0800

    add flask workflow

---
+++ Flask_React_Workflow/webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.(svg|png|jpg|jpeg|gif)$/,
+                loader: "file-loader",
+
+                options: {
+                    name: "[name].[ext]",
+                    outputPath: "../../static/dist",
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    output: {
+        path: __dirname + "/static/dist",
+        filename: "[name].bundle.js",
+    },
+};
